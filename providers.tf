a4f822b31861e8c916454a42d869c02e9bd899c4
Tom Stockwell <Tom.Stockwell@bailliegifford.com>
2025-03-28 15:06:23 +0000

    added deployment

--- providers.tf
+++ providers.tf
@@ -3,3 +3,10 @@ provider "azurerm" {
   subscription_id                 = "b133cf1b-9061-473a-a041-99c71f10c773"
   resource_provider_registrations = "none"
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
